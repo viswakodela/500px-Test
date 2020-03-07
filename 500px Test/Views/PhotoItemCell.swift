6fd41181694359b2540089569bfa6e4d472a16b6
Viswa Kodela <viswajith.kodela@ebox.ca>
2020-03-07 14:43:06 -0500

    UI changes for each cell. Add user information on top of the photo cell

--- 500px Test/Views/PhotoItemCell.swift
+++ 500px Test/Views/PhotoItemCell.swift
@@ -17,6 +17,10 @@ class PhotoItemCell: UICollectionViewCell {
             guard let photoUrl = photo?.imageUrls.first, let url = URL(string: photoUrl) else { return }
             photoImageView.sd_setImage(with: url, placeholderImage: nil)
             activitySpinner.stopAnimating()
+            if let userPicString = photo?.user?.userpic_url, let url = URL(string: userPicString) {
+                userImageView.sd_setImage(with: url)
+                userNameLabel.text = photo?.user?.username
+            }
         }
     }
     
@@ -29,11 +33,52 @@ class PhotoItemCell: UICollectionViewCell {
         return activityIndicator
     }()
     
-    let photoImageView: UIImageView = {
+    let userImageView: UIImageView = {
+        let userIV = UIImageView()
+        userIV.translatesAutoresizingMaskIntoConstraints = false
+        userIV.contentMode = .scaleAspectFit
+        userIV.clipsToBounds = true
+        userIV.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        userIV.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        return userIV
+    }()
+    
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Viswa Kodela"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var userInfoStackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [userImageView, userNameLabel])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.spacing = 10
+        sv.alignment = .center
+        return sv
+    }()
+    
+    let gradient = CAGradientLayer()
+    
+    lazy var photoImageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
+        
+        gradient.locations = [0.7, 1]
+        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradient.frame = iv.bounds
+        iv.layer.addSublayer(gradient)
+        
+        iv.addSubview(userInfoStackView)
+        userInfoStackView.leadingAnchor.constraint(equalTo: iv.leadingAnchor, constant: 6).isActive = true
+        userInfoStackView.bottomAnchor.constraint(equalTo: iv.bottomAnchor, constant: -6).isActive = true
+        userInfoStackView.trailingAnchor.constraint(equalTo: iv.trailingAnchor, constant: -6).isActive = true
+        userInfoStackView.heightAnchor.constraint(equalToConstant: 30).isActive = true
         return iv
     }()
     
@@ -43,16 +88,23 @@ class PhotoItemCell: UICollectionViewCell {
         configureCell()
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        userImageView.layer.cornerRadius = userImageView.bounds.height / 2
+        gradient.frame = photoImageView.bounds
+    }
+    
     private func configureCell() {
+        
+        addSubview(activitySpinner)
+        activitySpinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activitySpinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
         addSubview(photoImageView)
         photoImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         photoImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         photoImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         photoImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        
-        addSubview(activitySpinner)
-        activitySpinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        activitySpinner.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
     
     required init?(coder: NSCoder) {
